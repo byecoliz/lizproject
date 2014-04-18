743c12f420e129d1f3cf58cbded7eed74e410a95
finde <finde@ilumy.com>
2014-04-18 08:24:56 +0200

    Fixed issue about pagination by using session Replaced static project root in URL by using base_url()

--- application/controllers/Menu_Patient.php
+++ application/controllers/Menu_Patient.php
@@ -1,44 +1,38 @@
 <?php
-class Menu_Patient	extends CI_Controller 
+class Menu_Patient extends CI_Controller
 {
- function __construct()
- {
-  parent::__construct();
- session_start();
- }
- 
-
- function index($page = 'menu')
-   {	
-   
-	$data['title'] = ucfirst($page);
-	$data['main_content'] = 'pages/Menu_Patient';
-	
-	$this->load->model('Menu_model');
-    $data['rows']= $this->Menu_model->Planned_Hospitalization();
-   	
-   $this->load->model('Menu_model');
-   $data['count']= $this->Menu_model->waiting_list_count();
-    //var_dump($data);
-   //echo 'Success';
-  $this->load->view('templates/template',$data); 
-   }  
- 
-  
-function logout()
- {
- if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    $this->session_destroy();
-   redirect('login', 'refresh');
- }
+    function __construct()
+    {
+        parent::__construct();
+    }
 
 
+    function index($page = 'menu')
+    {
+
+        $data['title']        = ucfirst($page);
+        $data['main_content'] = 'pages/Menu_Patient';
+
+        $this->load->model('Menu_model');
+        $data['rows'] = $this->Menu_model->Planned_Hospitalization();
+
+        $this->load->model('Menu_model');
+        $data['count'] = $this->Menu_model->waiting_list_count();
+        //var_dump($data);
+        //echo 'Success';
+        $this->load->view('templates/template', $data);
+    }
 
 
-
-
-
+    function logout()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this->session_destroy();
+        }
+        redirect('login', 'refresh');
+    }
 
 
 }
+
 ?>
